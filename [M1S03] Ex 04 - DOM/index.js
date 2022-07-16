684bdec426b70ee1338eda919897afcdaeb2f3d5
Felipe Borba <ofelipeborba@gmail.com>
2022-07-16 13:01:34 -0300

    atualiza 1.0 ex 4 da lista de tarefas semana 3

--- [M1S03] Ex 04 - DOM/index.js
+++ [M1S03] Ex 04 - DOM/index.js
@@ -2,26 +2,16 @@
 // 5 textos quaisquer, um ao lado outro, com a cor azul (arquivo css).
 
 var minhaDiv
-const txt1 = "Sou o texto 1 e os outros textos irão aparecer, aguarde. ;",
-  txt2 = " Sou o texto 2 e apareci depois de 3s ;",
-  txt3 = " Sou o texto 3 e apareci depois de 5s ;",
-  txt4 = " Sou o texto 4 e apareci depois de 2s ;",
-  txt5 = " Sou o último texto e apareci depois de 1s ;";
-function mostrarTextosNaTela() {
- minhaDiv = document.getElementById("souUmaDiv")
- minhaDiv.style.color = "blue"
- minhaDiv.innerHTML = txt1;
+const txt1 = "Sou o texto 1;",
+  txt2 = " Sou o texto 2;",
+  txt3 = " Sou o texto 3;",
+  txt4 = " Sou o texto 4;",
+  txt5 = " Sou o último texto ;";
 
-setTimeout(function(){
-  minhaDiv.innerHTML += txt2;
-  setTimeout(function(){
-    minhaDiv.innerHTML += txt3;
-    setTimeout(function(){
-      minhaDiv.innerHTML += txt4;
-      setTimeout(function(){
-        minhaDiv.innerHTML += txt5;
-      }, 1000);
-    }, 2000);
-  }, 5000);
-}, 3000);
-}
+
+ minhaDiv = document.getElementById("souUmaDiv")
+ minhaDiv.innerHTML += txt1
+ minhaDiv.innerHTML += txt2
+ minhaDiv.innerHTML += txt3
+ minhaDiv.innerHTML += txt4
+ minhaDiv.innerHTML += txt5

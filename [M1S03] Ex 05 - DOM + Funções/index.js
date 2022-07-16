8310822e62f8a905f8a3b12e37510fddeb7e8de2
Felipe Borba <ofelipeborba@gmail.com>
2022-07-16 13:04:58 -0300

    Atualiza 1.0 ex 5 da lista de tarefas semana 3

--- [M1S03] Ex 05 - DOM + Funções/index.js
+++ [M1S03] Ex 05 - DOM + Funções/index.js
@@ -1,5 +1,4 @@
-// Crie um site (arquivo HTML) que, ao ser renderizado, adicione (arquivo JS)
-// 5 textos quaisquer, um ao lado outro, com a cor azul (arquivo css).
+// Substitua as repetições do exercício anterior por uma função.
 
 var minhaDiv
 const txt1 = "Sou o texto 1 e os outros textos irão aparecer, aguarde. ;",
@@ -9,7 +8,6 @@ const txt1 = "Sou o texto 1 e os outros textos irão aparecer, aguarde. ;",
   txt5 = " Sou o último texto e apareci depois de 1s ;";
 function mostrarTextosNaTela() {
  minhaDiv = document.getElementById("souUmaDiv")
- minhaDiv.style.color = "blue"
  minhaDiv.innerHTML = txt1;
 
 setTimeout(function(){
